7924623af5131ce9bb69be73af57b44d5694d75f
zeze <89672780+mdmdr8@users.noreply.github.com>
2022-12-10 14:26:50 +0900

    first commit

---
+++ src/main/java/kfq/cookcrew/CookcrewApplication.java
@@ -0,0 +1,13 @@
+package kfq.cookcrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookcrewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookcrewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kfq/cookcrew/CookcrewApplicationTests.java
@@ -0,0 +1,13 @@
+package kfq.cookcrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookcrewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
