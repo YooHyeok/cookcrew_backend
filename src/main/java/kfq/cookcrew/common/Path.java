2cc571299ffcbd7a2c010500ed17c9d666772ad7
92Lee92 <devindeed92@gmail.com>
2022-12-23 15:47:34 +0900

    Path 클래스 작성

---
+++ src/main/java/kfq/cookcrew/common/Path.java
@@ -0,0 +1,11 @@
+package kfq.cookcrew.common;
+
+public class Path {
+    //Path클래스 기본 인스턴스 생성자 호출 금지
+    private Path() {
+    }
+
+    public static final String RECIPEIMAGE = "C:/cookCrewTemp/recipe_image/";
+    public static final String RECIPETHUMBNAIL = "C:/cookCrewTemp/recipe_thumbnail/";
+    public static final String USERPROFILE = "C:/cookCrewTemp/user_profile/";
+}
