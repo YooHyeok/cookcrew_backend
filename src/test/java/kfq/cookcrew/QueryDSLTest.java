028729a2d11765d0a9950d39e7442162f6bdc88e
YooHyeok <yjk7454@naver.com>
2023-02-09 21:51:10 +0900

    queryDSL Setting & Test SuccessFull

---
+++ src/test/java/kfq/cookcrew/QueryDSLTest.java
@@ -0,0 +1,34 @@
+package kfq.cookcrew;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import kfq.cookcrew.reciepe.QRecipe;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class QueryDSLTest {
+    @Autowired
+    JPAQueryFactory jpaQueryFactory;
+
+    @Test
+    void queryDslFactoryTest(){
+        QRecipe qrecipe = QRecipe.recipe;
+        List<String> recipeList = jpaQueryFactory
+                .select(qrecipe.title)
+                .from(qrecipe)
+                .where(qrecipe.title.like("%김치%"))
+                .orderBy(qrecipe.cnt.asc())
+                .fetch();
+
+        for (String recipe : recipeList) {
+            System.out.println("recipe Title : " + recipe);
+        }
+    }
+
+}
